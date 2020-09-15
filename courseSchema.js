7fb08ffc67e60d1751ac172575a3ddc2c63a65d1
Edward Wong <edwardlm.wong@gmail.com>
2020-09-14 23:36:57 -0400

    Schema stitching with Federation.

--- courseSchema.js
+++ courseSchema.js
@@ -2,23 +2,36 @@ import { gql } from "apollo-server-express";
 
 export const typeDefs = gql`
   extend type Query {
-    course(id: Int!): Course
+    course(id: ID!): Course
     courses: [Course]
   }
 
-  type Course {
-    id: Int
+  type Course @key(fields: "id") {
+    id: ID!
     name: String!
+    students: [Student]
+  }
+
+  extend type Student @key(fields: "id") {
+    id: ID! @external
   }
 `;
 
 export const resolvers = {
+  Course: {
+    __resolveReference(ref) {
+      return { id: ref.id, name: "hello course" };
+    },
+    students(course) {
+      return course.students.map((id) => ({ __typename: "Student", id }));
+    },
+  },
   Query: {
     course: async (_, { id }, context) => {
-      return { id, name: "hello course" };
+      return { id, name: "hello course", students: [1, 2, 3] };
     },
     courses: async (_, {}, context) => {
-      return [{ id: 1, name: "hello course" }];
+      return [{ id: 1, name: "hello course", students: [1, 2, 3] }];
     },
   },
 };
--- studentSchema.js
+++ studentSchema.js
@@ -2,23 +2,37 @@ import { gql } from "apollo-server-express";
 
 export const typeDefs = gql`
   extend type Query {
-    student(id: Int!): Student
+    student(id: ID!): Student
     students: [Student]
   }
 
-  type Student {
-    id: Int
+  type Student @key(fields: "id") {
+    id: ID!
     name: String!
+    courses: [Course]
+  }
+
+  extend type Course @key(fields: "id") {
+    id: ID! @external
   }
 `;
 
 export const resolvers = {
+  Student: {
+    courses(student) {
+      return student.courses.map((id) => ({ __typename: "Course", id }));
+    },
+    __resolveReference(ref) {
+      return { id: ref.id, name: "hello student" };
+    }
+  },
+
   Query: {
     student: async (_, { id }, context) => {
-      return { id, name: "hello student" };
+      return { id, name: "hello student", courses: [1, 2, 3] };
     },
     students: async (_, {}, context) => {
-      return [{ id: 1, name: "hello student" }];
+      return [{ id: 1, name: "hello student", courses: [1] }];
     },
   },
 };
